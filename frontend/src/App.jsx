4e0bc06a4edac0dc4f59349e751eb7253584d650
Chaitanya_Kulkarni <kulkarnichaitanya001@gmail.com>
2025-03-26 14:14:46 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Tailwind CSS is Working!</h1>
+      <h1 className="text-4xl ">Tailwind CSS is Working!</h1>
     </div>
   );
 }
